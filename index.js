4e07ba3b595b0d818578353f95d177ada5ec152c
Lucas Vieira <lucasltv@gmail.com>
2022-05-18 21:00:15 -0300

    Create index.js
    Trigger function

---
+++ index.js
@@ -0,0 +1,43 @@
+// 1. Copy and paste this code in your Atlas Realm Function
+// 2. Replace with your send grid key
+// 3. Create a new trigger with "insert" event for your desired collection (ie: emails)
+// 4. Test creating some dynamic template in your send grid account (https://mc.sendgrid.com/dynamic-templates)
+
+const atlasCuster = '<replace_with_your_cluster_name>';
+const sendGridKey = 'replace_with_your_send_grid_key'
+
+exports = async function(event){
+  const sendgrid = require('@sendgrid/mail');
+  sendgrid.setApiKey(sendGridKey);
+  const {fullDocument, ns} = event;
+  const {templateId, to = [], templateData} = fullDocument;
+  const { db, coll } = ns;
+  const collection = context.services.get(atlasCuster).db(db).collection(coll);
+
+  try{
+    const msg= {
+      from: 'My email <email@example.com>',
+      templateId,
+      personalizations: [
+        {
+          to: to.map(x => ({email:x})), // type of 'to': string[]
+          dynamicTemplateData: templateData
+        },
+      ],
+    };
+    const sentResponse = await sendgrid.send(msg);
+    
+    collection.updateOne({_id:fullDocument._id}, {$set:{
+      sent:true,
+      sentAt: new Date(),
+      sentResponse
+    }})
+    
+  } catch(err){
+    console.error(err);
+    collection.updateOne({_id:fullDocument._id}, {$set:{
+      error:true,
+      errorDetails: err
+    }})
+  }
+};
